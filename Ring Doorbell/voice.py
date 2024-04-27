cd3559ad4c255d407863b4228450ef243b5a5084
KylePolson1 <kylekpolson@gmail.com>
2024-04-27 13:31:52 -0500

    push to talk

--- Ring Doorbell/voice.py
+++ Ring Doorbell/voice.py
@@ -1,37 +1,44 @@
 from random import randint
-
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 import pyaudio
-
-
+import keyboard
 
 class Client(DatagramProtocol):
+    def __init__(self):
+        super().__init__()
+        self.isPi = True
+
     def startProtocol(self):
         py_audio = pyaudio.PyAudio()
         self.buffer = 1024  # 127.0.0.1
         self.another_client = input("Write address: "), int(input("Write port: "))
-
         self.output_stream = py_audio.open(format=pyaudio.paInt16,
-                                           output=True, rate=44100, channels=2,
+                                           output=True, rate=44100, channels=1,
                                            frames_per_buffer=self.buffer)
         self.input_stream = py_audio.open(format=pyaudio.paInt16,
-                                          input=True, rate=44100, channels=2,
+                                          input=True, rate=44100, channels=1,
                                           frames_per_buffer=self.buffer)
         reactor.callInThread(self.record)
 
     def record(self):
         while True:
-            data = self.input_stream.read(self.buffer)
-            self.transport.write(data, self.another_client)
+            if self.isPi or keyboard.is_pressed('t'):
+                #print(self.isPi, keyboard.is_pressed('t'))
+                data = self.input_stream.read(self.buffer)
+                self.transport.write(data, self.another_client)
 
     def datagramReceived(self, datagram, addr):
         self.output_stream.write(datagram)
+
+    def toggleIsPi(self):
+        self.isPi = not self.isPi
 
 
 if __name__ == '__main__':
     port = randint(1000, 3000)
     print("Working on port: ", port)
-
-    reactor.listenUDP(port, Client())
+    cli = Client()
+    cli.toggleIsPi()  # Defaults to true
+    reactor.listenUDP(port, cli)
     reactor.run()
